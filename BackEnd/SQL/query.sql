18152dc979ff73bf9aae031c8ee2e4983132197d
taus-if <65533241+taus-if@users.noreply.github.com>
2022-06-07 09:40:10 +0600

    query updated

--- BackEnd/SQL/query.sql
+++ BackEnd/SQL/query.sql
@@ -8,14 +8,17 @@ SELECT di.dealer_id,organization_name,applicant_name,email,tin_number,da.area_co
     from dealer_info di, dealer_area da, distribution_area dia
     where di.dealer_id=da.dealer_id AND da.area_code=dia.area_code
 
+--Family query
+SELECT member_nid, member_name, member_occupation, member_income, family_info.nid, customer_info.name from family_info,customer_info
+where family_info.nid=customer_info.nid;
+
 --total query
-SELECT ci.tcb_card_no "TCB Card NO.",ci.area_code "Area CODE",ci.package_no "Package",ci.name "Name",ci.gender "Sex" ,ci.age "Age",ce.totalspent "Total Spending Money",g.item_name"Items"
-    di.dealer_id "Dealer ID" ,di.applicant_name "Dealer Name"
+SELECT ci.tcb_card_no "TCB Card NO.",ci.area_code "Area CODE",ci.package_no "Package",ci.name "Name",ci.gender "Sex" ,ci.age "Age",ce.totalspent "Total Spending Money",g.item_name"Items" ,di.dealer_id "Dealer ID" ,di.applicant_name "Dealer Name"
 
     FROM admin a, package p, dealer_area da, items_distributed id, dealer_info di, customer_expenditure ce, goods g, family_info fi, customer_info ci, distribution_area dia
         
     WHERE fi.member_nid=ci.nid AND ci.nid=ce.nid AND ce.item_name=g.item_name AND g.item_name=id.item_name AND 
         id.dealer_id=di.dealer_id AND di.dealer_id=da.dealer_id AND da.area_code=dia.area_code AND dia.area_code=ci.area_code
-        AND ci.package_no=p.package_no 
+        AND ci.package_no=p.package_no ;
     
     

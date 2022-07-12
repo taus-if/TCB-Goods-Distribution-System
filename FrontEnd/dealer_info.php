c993a2fb594247ca38ca1b526cc51b30427a43c6
unknown <rifat010bushra@gmail.com>
2022-07-12 13:25:38 +0600

    dealer info search functionabled

--- FrontEnd/dealer_info.php
+++ FrontEnd/dealer_info.php
@@ -96,7 +96,7 @@ if (!$conn) {
                         <div class="row">
                             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                                 <div class="page-header">
-                                    <h2 class="pageheader-title" style="text-align: center;">DEALER INFORMATION</h2>
+                                    <h2 class="pageheader-title" style="text-align: center;">DEALER's INFORMATION</h2>
                                     <div>
                                         <div class="page-breadcrumb">
                                             <nav aria-label="breadcrumb">
@@ -107,13 +107,34 @@ if (!$conn) {
                                             </nav>
                                         </div>
                                         <div class="main-content container-fluid p-0" class="col-lg-12">
-                                            <div class="email-search">
-                                                <div class="input-group input-search">
-                                                    <input class="form-control" type="text" placeholder="Search in Result Register..."><span class="input-group-btn">
-                                                        <button class="btn btn-secondary" type="button"><i class="bi bi-search"></i></button></span>
-                                                </div>
+                                            <div class="search">
+                                                <!-- <form method="post"> -->
+                                                    <!-- <div class="input-group input-search">
+                                                        <input class="form-control" type="search" id="res" name="res" placeholder="Search by any...">
+                                                        <span class="input-group-btn">
+                                                            <button class="btn btn-secondary" type="button"><i class="bi bi-search" style="color:#38CE24 ;"></i></button>
+                                                        </span>
+                                                    </div> -->
+                                                <!-- </form> -->
+                                                <form method="post" >
+                                                    <input class="col-lg-11" type="text" name="search" placeholder="Search by any">
+                                                    <input type="submit" name="submit">
+                                                </form>
+
+                                                <?php
+
+                                                ?>
                                             </div>
                                         </div>
+                                        <!-- <div class="input-group container-fluid p-0 col-lg-12">
+                                            <div class="form-outline">
+                                                <input id="search-input" type="search" id="form1" class="form-control" />
+                                                <label class="form-label" for="form1">Search</label>
+                                            </div>
+                                            <button id="search-button" type="button" class="btn btn-primary">
+                                                <i class="bi bi-search"></i>
+                                            </button>
+                                        </div> -->
                                     </div>
                                 </div>
                             </div>
@@ -143,16 +164,48 @@ if (!$conn) {
                             </thead>
                             <tbody>
                                 <?php
-                                $sql = "select * from  dealer_info, info where dealer_info.dealer_id=info.dealer_id  ";  //username = '$uname';
-                                $stid = oci_parse($conn, $sql);
-                                $r = oci_execute($stid);
-                                
-                                // <td>" . $row["organization_name"] . $row["organization_address"] . $row["tin_number"] . "</td>
-                                // <td>12/05/22</td>
+                                if (isset($_POST["submit"])) {
 
-                                while ($row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) {
-                                    echo
-                                    "<tr>
+                                    $name = $_POST["search"];
+                                    $result = "SELECT * FROM dealer_info WHERE applicant_name LIKE '%{$name}%' OR email LIKE '%{$name}%' 
+                                                            OR dealer_id LIKE '%{$name}%' OR organization_name LIKE '%{$name}%' OR 
+                                                            permanent_address LIKE '%{$name}%' ";
+
+                                    $stidd = oci_parse($conn, $result);
+                                    $rr = oci_execute($stidd);
+                                    while ($row = oci_fetch_array($stidd, OCI_ASSOC + OCI_RETURN_NULLS)) {
+                                        echo "<tr>
+                                        <td>" . $row["APPLICANT_NAME"] . "</td>
+                                        <td>" . $row["PERMANENT_ADDRESS"] . "</td>
+                                        <td>" . $row["DATE_OF_BIRTH"] . "</td>
+                                        <td>" . $row["EMAIL"] . "</td>
+                                        <td>" . $row["DEALER_ID"] . "</td>  
+                                        
+                                    </tr>";
+                                    }
+
+                                    // while ($row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) {
+                                    //     echo
+                                    //     "<tr>
+                                    //         <td>" . $row["APPLICANT_NAME"] . "</td>
+                                    //         <td>" . $row["PERMANENT_ADDRESS"] . "</td>
+                                    //         <td>" . $row["DATE_OF_BIRTH"] . "</td>
+                                    //         <td>" . $row["EMAIL"] . "</td>
+                                    //         <td>" . $row["DEALER_ID"] . "</td>  
+
+                                    //     </tr>";
+                                    // }
+                                } else {
+                                    $sql = "select * from  dealer_info, info where dealer_info.dealer_id=info.dealer_id  ";  //username = '$uname';
+                                    $stid = oci_parse($conn, $sql);
+                                    $r = oci_execute($stid);
+
+                                    // <td>" . $row["organization_name"] . $row["organization_address"] . $row["tin_number"] . "</td>
+                                    // <td>12/05/22</td>
+
+                                    while ($row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) {
+                                        echo
+                                        "<tr>
                                             <td>" . $row["APPLICANT_NAME"] . "</td>
                                             <td>" . $row["PERMANENT_ADDRESS"] . "</td>
                                             <td>" . $row["DATE_OF_BIRTH"] . "</td>
@@ -160,6 +213,7 @@ if (!$conn) {
                                             <td>" . $row["DEALER_ID"] . "</td>  
                                             
                                         </tr>";
+                                    }
                                 }
                                 ?>
                             </tbody>

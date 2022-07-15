40f25b62d6975db51a3a23f9302a1f25eed568f6
adretokhan <adreto.khan@gmail.com>
2022-07-15 10:37:10 -0700

    storage updated

--- FrontEnd/admin.php
+++ FrontEnd/admin.php
@@ -101,7 +101,7 @@ if (!$conn) {
 
           <div class="col-md-3 col-lg-3 ">
             <div class="card d-flex align-items-stretch outterround">
-              <a href="storage.html" class="stretched-link"></a>
+              <a href="storage.php" class="stretched-link"></a>
               <i class="bi bi-building custom-icon d-flex justify-content-center"></i>
               <div class="card_title d-flex justify-content-center">
                 <p>Storage</p>
